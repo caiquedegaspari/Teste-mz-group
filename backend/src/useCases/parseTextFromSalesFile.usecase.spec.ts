a6b605cb245a9a7d31de740956968052a2f9369d
caiquedegaspari <caique.degaspare@gmail.com>
2023-04-04 20:19:19 -0300

    test: test invalid parameter

--- backend/src/useCases/parseTextFromSalesFile.usecase.spec.ts
+++ backend/src/useCases/parseTextFromSalesFile.usecase.spec.ts
@@ -1,5 +1,18 @@
-describe('First test', () => {
-  it('Should be able to test', () => {
-    expect(1 + 1).toBe(2)
+import { ISalesHistoryRepository } from "../repositories/ISalesHistory.repository"
+import { ParseTextFromSalesFileUseCase } from "./parseTextFromSalesFile.usecase"
+
+const salesHistoryRepository: ISalesHistoryRepository = {
+  save: jest.fn()
+}
+
+let sut: ParseTextFromSalesFileUseCase
+
+beforeEach(() => {
+  sut = new ParseTextFromSalesFileUseCase(salesHistoryRepository)
+})
+
+describe('Test ParseTextFromSalesFileUseCase', () => {
+  it('Should be throw error with wrong param', async () => {
+    await expect(sut.execute('')).rejects.toThrowError()
   })
 })
