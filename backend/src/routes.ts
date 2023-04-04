5f78235fbe7f626e93f7c3c2323124cf862911da
caiquedegaspari <caique.degaspare@gmail.com>
2023-04-04 16:13:08 -0300

    feat: add multer and uploadFile route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,31 @@
-import { Response, Router } from "express";
+import { Request, Response, Router } from "express";
 import { FirstUseCase } from "./useCases/first-usecase";
 import { FirstUsecaseController } from "./useCases/first-usecase.controller";
+import multer from 'multer'
 
 const router = Router()
 
 const firstUsecaseController = new FirstUsecaseController()
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+
+router.post("/uploadFile", upload.single("sellsFile"), (req: Request, res: Response) => {
+  const file = req.file;
+
+  if (!file) { 
+    throw new Error("Please upload a file");
+  }
+  if(file?.buffer) {
+    const multerText = Buffer.from(file?.buffer).toString("utf-8");
+
+  const result = {
+    fileText: multerText,
+  };
+
+  res.send(result);
+  }
+});
 
 router.get('/api', firstUsecaseController.handle)
 
