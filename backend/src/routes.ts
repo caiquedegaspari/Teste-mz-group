869d270c88b93fdff2576a87543bf41e068f3ecd
caiquedegaspari <caique.degaspare@gmail.com>
2023-04-04 16:35:35 -0300

    refact: routes.ts

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,32 +1,14 @@
 import { Request, Response, Router } from "express";
-import { FirstUseCase } from "./useCases/first-usecase";
-import { FirstUsecaseController } from "./useCases/first-usecase.controller";
+import { UploadSalesFileController } from "./controllers/uploadSalesFile.controller";
 import multer from 'multer'
 
 const router = Router()
 
-const firstUsecaseController = new FirstUsecaseController()
+const uploadSellsFileController = new UploadSalesFileController()
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage })
 
-router.post("/uploadFile", upload.single("sellsFile"), (req: Request, res: Response) => {
-  const file = req.file;
-
-  if (!file) { 
-    throw new Error("Please upload a file");
-  }
-  if(file?.buffer) {
-    const multerText = Buffer.from(file?.buffer).toString("utf-8");
-
-  const result = {
-    fileText: multerText,
-  };
-
-  res.send(result);
-  }
-});
-
-router.get('/api', firstUsecaseController.handle)
+router.post("/uploadFile", upload.single("sellsFile"), uploadSellsFileController.handle);
 
 export { router }
