a378f7bf42ddeb335dbd4b680a7f094a3cb859f3
caiquedegaspari <caique.degaspare@gmail.com>
2023-04-06 23:59:48 -0300

    feat: add listSales controller/route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,17 @@
 import { Request, Response, Router } from "express";
 import { UploadSalesFileController } from "./controllers/uploadSalesFile.controller";
 import multer from 'multer'
+import { ListSalesController } from "./controllers/listSales.controller";
 
 const router = Router()
 
 const uploadSellsFileController = new UploadSalesFileController()
+const listSalesController = new ListSalesController()
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage })
 
 router.post("/uploadFile", upload.single("salesFile"), uploadSellsFileController.handle);
+router.get("/listSales", listSalesController.handle);
 
 export { router }
